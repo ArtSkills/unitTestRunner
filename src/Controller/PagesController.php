bebb2378c258632a460ac08578cc68a0aba51aea
vovantune <tune@artskills.ru>
2017-01-27 11:11:50 +0300

    заглушка-редирект

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+class PagesController extends AppController
+{
+	/** Заглушка */
+	public function display() {
+		$this->redirect('https://github.com/ArtSkills');
+	}
+}
