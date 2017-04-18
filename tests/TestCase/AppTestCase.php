7da8c0d780c5db4eb9a43e9253048d46042c6726
Vladimir <tune@artskills.ru>
2017-04-18 12:48:42 +0300

    обновки для common

--- tests/TestCase/AppTestCase.php
+++ tests/TestCase/AppTestCase.php
@@ -1,33 +1,7 @@
 <?php
 namespace App\Test\TestCase;
 
-use App\Test\TestCaseTrait;
-use Cake\TestSuite\TestCase;
-
-abstract class AppTestCase extends TestCase
+abstract class AppTestCase extends \ArtSkills\TestSuite\AppTestCase
 {
-	use TestCaseTrait;
-
-	/**
-	 * @inheritdoc
-	 */
-	public function setUp() {
-		parent::setUp();
-		$this->_setUp();
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	public function tearDown() {
-		parent::tearDown();
-		$this->_tearDown();
-	}
-
-	/** @inheritdoc */
-	public static function setUpBeforeClass() {
-		parent::setUpBeforeClass();
-		self::_setUpBeforeClass();
-	}
 
 }
--- tests/TestCase/Controller/AppControllerTestCase.php
+++ tests/TestCase/Controller/AppControllerTestCase.php
@@ -1,32 +1,13 @@
 <?php
 namespace App\Test\TestCase\Controller;
 
-use App\Test\TestCaseTrait;
-use ArtSkills\TestSuite\IntegrationTestCase;
-
-
-abstract class AppControllerTestCase extends IntegrationTestCase
+abstract class AppControllerTestCase extends \ArtSkills\TestSuite\AppControllerTestCase
 {
-	use TestCaseTrait;
-
 	/**
 	 * @inheritdoc
 	 */
 	public function setUp() {
 		$this->configApplication(null, [CONFIG]);
 		parent::setUp();
-		$this->_setUp();
-	}
-
-	/** @inheritdoc */
-	public function tearDown() {
-		parent::tearDown();
-		$this->_tearDown();
-	}
-
-	/** @inheritdoc */
-	public static function setUpBeforeClass() {
-		parent::setUpBeforeClass();
-		self::_setUpBeforeClass();
 	}
 }
