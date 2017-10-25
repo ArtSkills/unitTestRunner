66a83bb4b0bb0d22ca736c035da5b1b81726119d
Vladimir <tune@artskills.ru>
2017-10-25 15:50:12 +0300

    Тесты починил, проверка на наличие нотисов

--- tests/TestCase/AppTestCase.php
+++ tests/TestCase/AppTestCase.php
@@ -1,7 +1,13 @@
 <?php
 namespace App\Test\TestCase;
 
+use Cake\Core\Configure;
+
 abstract class AppTestCase extends \ArtSkills\TestSuite\AppTestCase
 {
-
+	/** @inheritdoc */
+	public function setUp() {
+		Configure::write('serverId', 'PHP7');
+		parent::setUp();
+	}
 }
